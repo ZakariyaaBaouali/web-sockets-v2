974e432b3c507907908f2f45e69aee478b1ec983
zakaria <zakariabaouali@outlook.com>
2024-06-27 09:43:47 +0100

    add some new socket features

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,19 @@ import express from "express";
 import { Server, Socket } from "socket.io";
 import { createServer } from "http";
 import cors from "cors";
+import { PORT } from "./config";
 
 //🚀
 const app = express();
+app.use(express.json());
 const server = createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+    credentials: true,
+  },
+});
 
 //🚀
 app.use(express.json());
@@ -14,13 +22,48 @@ app.use(
   cors({
     origin: "*",
     methods: ["GET", "POST", "PATCH", "DELETE"],
+    credentials: true,
   })
 );
+
+interface ISocket {
+  id: string;
+  message: string;
+}
+
+//midds
+io.use((socket, next) => {
+  next();
+});
 
 //manage events 🚀🚀
 io.on("connection", (socket: Socket) => {
   //
   console.log(`client connected with id : ${socket.id}`);
+
+  //
+  socket.on("message", (data) => {
+    io.emit("new-message", data);
+  });
+
+  socket.on("send-message", (data: any) => {
+    const { id, message } = JSON.parse(data) as ISocket;
+    //id = { id , roomName }
+    console.log(message);
+    //socket.broadcast.emit("new-message", id);
+    //io.to(id).emit("new-message", message);
+    socket.to(id).emit("new-message", message);
+  });
+
+  socket.on("join-room", (roomName: string) => {
+    //
+    console.log(`User id = ${socket.id} ,joined ${roomName} room`);
+    socket.join(roomName);
+  });
+
+  socket.on("leave-room", (roomName: string) => {
+    socket.leave(roomName);
+  });
 
   socket.on("disconnect", () => {
     console.log("client disconnect");
@@ -28,6 +71,6 @@ io.on("connection", (socket: Socket) => {
 });
 
 //🚀🚀
-server.listen(8000, () => {
-  console.log(`Sever running on port ${8000}`);
+server.listen(PORT, () => {
+  console.log(`Sever running on port ${PORT}`);
 });
